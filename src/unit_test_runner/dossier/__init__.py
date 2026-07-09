b111f62b89a791442d343066bdcfbee513043a88
katamor1 <a_mysterious_knight@yahoo.co.jp>
2026-07-10 01:29:21 +0900

    Route dossier analysis through quick summary writer

--- src/unit_test_runner/dossier/__init__.py
+++ src/unit_test_runner/dossier/__init__.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
 from .finalizer import finalize_function_dossier, prepare_review_from_dossier
+from .quick_summary_compat import analyze_function_workflow
 from .workflow import (
-    analyze_function_workflow,
     generate_build_workspace_from_reports,
     generate_build_workspace_from_workspace,
     generate_harness_skeleton_from_reports,
