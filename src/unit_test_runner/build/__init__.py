d28ac0b027b21bc39d2626d40fc9de3035925769
katamor1 <a_mysterious_knight@yahoo.co.jp>
2026-07-09 22:38:53 +0900

    Apply build workspace compatibility fixes

--- src/unit_test_runner/build/__init__.py
+++ src/unit_test_runner/build/__init__.py
@@ -1,4 +1,7 @@
 from .build_workspace_generator import generate_build_workspace
 from .log_parser import parse_build_log
+from .workspace_compat_fixes import apply_build_probe_compat_fixes
+
+apply_build_probe_compat_fixes()
 
 __all__ = ["generate_build_workspace", "parse_build_log"]
