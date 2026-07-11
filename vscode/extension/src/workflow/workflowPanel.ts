52dfb2e49f81d6ea33ab945d1e228eff5144858f
katamor1 <a_mysterious_knight@yahoo.co.jp>
2026-07-11 10:01:38 +0900

    Use shared workflow panel rendering policy

--- vscode/extension/src/workflow/workflowPanel.ts
+++ vscode/extension/src/workflow/workflowPanel.ts
@@ -1,39 +1,9 @@
-import * as vscode from 'vscode';
-
-import {
-  WorkflowPanelProvider as BaseWorkflowPanelProvider,
-  renderWorkflowHtml as renderBaseWorkflowHtml,
-} from './workflowPanelBase';
-
 export {
+  WorkflowPanelProvider,
+  renderWorkflowHtml,
+  resolveWorkflowActionPresentation,
+  resolveWorkflowReports,
   SIMPLE_SECONDARY_ACTIONS,
   SIMPLE_WORKFLOW_ACTIONS,
-  resolveWorkflowReports,
+  workflowStatusLabel,
 } from './workflowPanelBase';
-
-export class WorkflowPanelProvider extends BaseWorkflowPanelProvider {
-  private terminologyView?: vscode.WebviewView;
-
-  resolveWebviewView(webviewView: vscode.WebviewView): void {
-    this.terminologyView = webviewView;
-    super.resolveWebviewView(webviewView);
-  }
-
-  refresh(): void {
-    super.refresh();
-    if (this.terminologyView) {
-      this.terminologyView.webview.html = applyWorkflowViewTerminology(this.terminologyView.webview.html);
-    }
-  }
-}
-
-export function renderWorkflowHtml(
-  ...args: Parameters<typeof renderBaseWorkflowHtml>
-): ReturnType<typeof renderBaseWorkflowHtml> {
-  return applyWorkflowViewTerminology(renderBaseWorkflowHtml(...args));
-}
-
-function applyWorkflowViewTerminology(html: string): string {
-  const previousLabel = '\u5f93\u6765';
-  return html.split(previousLabel).join('詳細');
-}
