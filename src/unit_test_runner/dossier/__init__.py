da70c676103274e2b170f5b9d8f6c6461a457764
katamor1 <a_mysterious_knight@yahoo.co.jp>
2026-07-11 13:33:35 +0900

    Apply link-aware dossier workflow compatibility

--- src/unit_test_runner/dossier/__init__.py
+++ src/unit_test_runner/dossier/__init__.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
 from .finalizer import finalize_function_dossier, prepare_review_from_dossier
+from .link_context_compat import apply_link_context_compat
+
+apply_link_context_compat()
+
 from .quick_summary_compat import analyze_function_workflow
 from .workflow import (
     generate_build_workspace_from_reports,
